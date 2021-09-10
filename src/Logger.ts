c89e7b33e1f7cabf8ffcf9f2cc360f674d5a7967
ItsEcholot <me@echolot.io>
2021-09-10 13:50:33 +0200

    Default pretty log

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,5 +1,15 @@
 import Pino from 'pino';
 
+const transport = Pino.transport({
+  target: 'pino-pretty',
+  options: {
+    destination: 1,
+    colorize: true,
+    translateTime: 'SYS:standard',
+    ignore: 'pid,hostname'
+  }
+});
+
 export default Pino({
   level: process.env.LOG_LEVEL || 'info'
-});
+}, process.env.LOG_JSON === 'true' ? undefined : transport);
