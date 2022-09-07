97b329c6d2f3886967dec2093a1369556104f69c
Marc <me@echolot.io>
2022-09-07 10:39:22 +0200

    Add image copy command to multiarch build script

--- build-multiarch.sh
+++ build-multiarch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag ghcr.io/itsecholot/containernursery:multiarch .
+regctl image copy ghcr.io/itsecholot/containernursery:multiarch ghcr.io/itsecholot/containernursery:dev
