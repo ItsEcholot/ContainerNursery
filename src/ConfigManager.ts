ac1be55f1949d06caf038c688d248174379766fc
Samurai1201 <83700944+Samurai1201@users.noreply.github.com>
2023-12-08 18:45:04 +0100

    Added fancyName variable to ConfigManager
    Now the fancyName variable gets read and passed to proxyhost

--- src/ConfigManager.ts
+++ src/ConfigManager.ts
@@ -11,6 +11,7 @@ const placeholderServerListeningPort = 8080;
 type ProxyHostConfig = {
   domain: string | string[]
   containerName: string | string[]
+  fancyName: string | string[]
   proxyHost: string
   proxyPort: number
   proxyUseHttps: boolean
@@ -120,6 +121,9 @@ export default class ConfigManager {
           proxyHostConfig.containerName instanceof Array
             ? proxyHostConfig.containerName
             : [proxyHostConfig.containerName],
+          proxyHostConfig.fancyName instanceof Array
+            ? proxyHostConfig.fancyName
+            : [proxyHostConfig.fancyName],
           proxyHostConfig.proxyHost,
           proxyHostConfig.proxyPort,
           proxyHostConfig.timeoutSeconds
